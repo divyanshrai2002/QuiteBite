f85183da84e816649817b42c653c9c7b071db6c5
Divyansh kumar rai <152678885+divyanshrai2002@users.noreply.github.com>
2024-12-15 18:19:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
+const http = require('http');
+
 import app from "./app.js";
 
+const server = http.createServer(app);
 const PORT = process.env.PORT || 5000; // Default to 5000 if PORT is not set
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`SERVER HAS STARTED AT PORT ${PORT}`);
 });
 
